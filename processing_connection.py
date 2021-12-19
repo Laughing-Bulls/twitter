3c2656a55da92b0c495ed86fc77affbd047dd629
Laughing-Bulls <asd26@njit.edu>
2021-12-19 15:48:07 -0500

    Task: Refactor some code

--- processing_connection.py
+++ processing_connection.py
@@ -8,11 +8,21 @@ class TwitterIn:
     """ Get Twitter stream"""
 
     @staticmethod
-    def get_tweets(auth):
+    def create_twitter_url():
+        # set up twitter request parameters
         url = 'https://stream.twitter.com/1.1/statuses/filter.json'
-        query_data = [('language', 'en'), ('locations', '-130,-20,100,50'),('track','iphone')]
-        query_url = url + '?' + '&'.join([str(t[0]) + '=' + str(t[1]) for t in query_data])
+        # English tweets in the USA
+        query_data = [('language', 'en'), ('locations', '-160,17,-65,50'), ('track', 'iphone')]
+        query_url = url + '?' + '&'.join([str(element[0]) + '=' + str(element[1]) for element in query_data])
+        return query_url
+
+    @staticmethod
+    def get_tweets(auth):
+        # return response as serialized JSON content
+        query_url = TwitterIn.create_twitter_url()
         response = requests.get(query_url, auth=auth, stream=True)
+        if response:
+            print("THE TWITTER QUERY WAS SUCCESSFUL")
         print(query_url, response)
         return response
 
@@ -25,18 +35,18 @@ class TwitterIn:
                     # collect tweet information
                     tweet_data = json.loads(data)
                     tweet_text = tweet_data['text']
-                    twitterID = tweet_data['user']['screen_name']
+                    # twitterID = tweet_data['user']['screen_name']
                     tweet_source = tweet_data['source']
-                    dataSoup = BeautifulSoup(tweet_source)
-                    for anchor in dataSoup.find_all('a'):
+                    soup = BeautifulSoup(tweet_source)
+                    for anchor in soup.find_all('a'):
                        tweet_location = anchor.text
-                    source_device = tweet_location.replace(" ", "")
-                    device = "TS"+source_device.replace("Twitter", "")
+                    # source_device = tweet_location.replace(" ", "")
+                    # device = "TS"+source_device.replace("Twitter", "")
 
                     # location filter
                     tweet_country_code = "CC"+tweet_data['place']['country_code']
                     if tweet_country_code=="CCUS" and "tter" in tweet_location and "\n" not in tweet_text:
-                       iterations=iterations+1
+                       iterations += 1
                        print(tweet_text)
 
                        # delimiter for distinguishing tweet_data
